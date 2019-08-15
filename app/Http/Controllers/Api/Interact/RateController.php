8a42791810663b393cf0e023fb95be7d427c29ce
abdalgaderserag <abdalgadersir@gmail.com>
2019-08-15 11:37:50 -0700

    create the invoke for the rate controller

--- app/Http/Controllers/Api/Interact/RateController.php
+++ app/Http/Controllers/Api/Interact/RateController.php
@@ -2,19 +2,33 @@
 
 namespace App\Http\Controllers\Api\Interact;
 
+use App\Rate;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class RateController extends Controller
 {
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function __invoke(Request $request)
     {
-        //
+        $like = Rate::where('user_id', $request->user_id)->where('owner_id', Auth::id())->first();
+        if (!empty($like)) {
+            $like = new Rate();
+            $like->user_id = $request->user_id;
+            $like->owner_id = Auth::id();
+            $like->rate = $request->rate;
+            $like->save();
+            return response($like->rate, 200);
+        } else {
+            $like->rate = $request->rate;
+            $like->save();
+            return response($like->rate, 200);
+        }
     }
 }
