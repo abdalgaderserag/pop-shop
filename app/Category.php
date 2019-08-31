27cd73446fe5e56ef0fd128db9d743cb9aa0b3fe
abdalgaderserag <abdalgadersir@gmail.com>
2019-08-31 11:22:17 -0700

    convirte min

--- app/Category.php
+++ app/Category.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+
+    protected $casts = [
+        'exchangeable' => 'boolean',
+        'used' => 'boolean'
+    ];
+
     /**
      * Set a given attribute on the base type.
      *
--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -34,19 +34,43 @@
             </div>
 
             <div v-if="isLoaded" v-for="item in items" class="item-card flex-box">
-                <img src="/test/mapbox.jpg" class="item-image">
+                <div class="item-left">
+                    <img src="/test/mapbox.jpg" class="item-image">
+                    <div>
+                        {{ parseTime(item.created_at) }}
+                        <!--{{ item.created_at }}-->
+                    </div>
+                </div>
                 <div class="item-body">
                     <div class="flex-box item-text">
-                        <div style="width: 50%">
-                            <span>{{ item.title }}</span><span> - {{ '$' + item.budget }}</span></div>
+                        <div>
+                            <div style="font-size: 3.5vh">{{ item.title }}</div>
+                            <span> - {{ '$' + item.budget }}</span>
+                        </div>
                     </div>
                     <div class="item-text"><span>Location :</span>
                         <span>{{ item.category.location }}</span></div>
                     <div class="item-text"><span>Category :</span>
                         <span> {{item.category.base_type}} / {{ item.category.seconder_type }}</span></div>
-                    <div class="item-text">only <span>{{ item.stock }}</span> in stock</div>
+
                     <div class="item-text"><span>By</span> <a :href="'/profile/' + item.user.id" class="link-clear">
-                        {{item.user.name }}</a> at{{ item.created_at }}
+                        {{item.user.name }}</a>
+                    </div>
+
+                    <div v-if="item.stock != 'unlimited'" class="item-text">
+                        only <span>{{ item.stock }}</span> in stock
+                    </div>
+
+
+                    <br>
+                    <div v-if="item.stock == 'unlimited'" class="item-tag">
+                        Unlimited
+                    </div>
+                    <div v-if="item.category.exchangeable" class="item-tag">
+                        Exchangelable
+                    </div>
+                    <div v-if="item.category.used" class="item-tag">
+                        Used
                     </div>
                 </div>
             </div>
@@ -80,6 +104,23 @@
                     this.items = response.data.items;
                     this.isLoaded = true;
                 });
+            },
+            parseTime: function (time) {
+                let par = (new Date('' + time).getTime() / 1000) - (new Date().getTime() / 1000);
+                if (par < 60)
+                    return 'just now!';
+                else if (par < 3600)
+                    return Number.parseInt(par / 60);
+                else if (par < (3600 / 24)) {
+                    let hour = Number.parseInt((par / 3600) * 24);
+                    if (hour == 1)
+                        return 'hour ago';
+                    else if (hour == 2)
+                        return 'two hour ago';
+                    else
+                        return hour + ' hour ago';
+                } else
+                    return time.slice(0, time.length - 9);
             }
         }
     }
