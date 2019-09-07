240df761e7150bcb35f2a3f83465701df1a1f54e
abdalgaderserag <abdalgadersir@gmail.com>
2019-09-07 04:07:04 -0700

    create the dashboard

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -2,6 +2,118 @@
 
 @section('title',\Illuminate\Support\Facades\Auth::user()->name . ' | Dashboard')
 
-@section('content')
+@section('style')
+    <link rel="stylesheet" href="/css/parts/header.css">
+@endsection
 
+@section('content')
+    <div class="flex-box">
+        <div class="main-section" style="width: 100%">
+            <div>
+                <button class="input-button" style="width: 146px">+ Item</button>
+                {{--<div>New Sold Items:</div>--}}
+                {{--<div>--}}
+                    {{--<table></table>--}}
+                {{--</div>--}}
+                <div>Out Of stock:</div>
+                <div>
+                    <table style="background-color: white;width: 100%;">
+                        <tr>
+                            <td style="width: 24px">1</td>
+                            <td>chair</td>
+                            <td>$200</td>
+                            <td style="width: 24px">2</td>
+                            <td style="width: 24px">
+                                ^<br>
+                                2<br>
+                                ^
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 24px">1</td>
+                            <td>chair</td>
+                            <td>$200</td>
+                            <td style="width: 24px">2</td>
+                            <td style="width: 24px">
+                                ^<br>
+                                2<br>
+                                ^
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 24px">1</td>
+                            <td>chair</td>
+                            <td>$200</td>
+                            <td style="width: 24px">2</td>
+                            <td style="width: 24px">
+                                ^<br>
+                                2<br>
+                                ^
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 24px">1</td>
+                            <td>chair</td>
+                            <td>$200</td>
+                            <td style="width: 24px">2</td>
+                            <td style="width: 24px">
+                                ^<br>
+                                2<br>
+                                ^
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div>Items:</div>
+                <div>
+                    <table style="background-color: white;width: 100%;">
+                        <tr>
+                            <td style="width: 24px">1</td>
+                            <td>chair</td>
+                            <td>$200</td>
+                            <td style="width: 24px">2</td>
+                            <td style="width: 24px">
+                                ^<br>
+                                2<br>
+                                ^
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 24px">1</td>
+                            <td>chair</td>
+                            <td>$200</td>
+                            <td style="width: 24px">2</td>
+                            <td style="width: 24px">
+                                ^<br>
+                                2<br>
+                                ^
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 24px">1</td>
+                            <td>chair</td>
+                            <td>$200</td>
+                            <td style="width: 24px">2</td>
+                            <td style="width: 24px">
+                                ^<br>
+                                2<br>
+                                ^
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 24px">1</td>
+                            <td>chair</td>
+                            <td>$200</td>
+                            <td style="width: 24px">2</td>
+                            <td style="width: 24px">
+                                ^<br>
+                                2<br>
+                                ^
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
