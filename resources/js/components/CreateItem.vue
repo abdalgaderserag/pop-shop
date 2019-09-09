a1e1f523e4676bed9c464ecba16ba6a5e9a112c5
abdalgaderserag <abdalgadersir@gmail.com>
2019-09-09 12:24:12 -0700

    some view changes

--- resources/js/components/CreateItem.vue
+++ resources/js/components/CreateItem.vue
@@ -2,59 +2,71 @@
     <div class="flex-box" style="margin: 0 20%">
         <div class="main-section" style="width: 100%">
             <!--<div>Create Item:</div>-->
-            <div>
-                <div>item title<span class="red">*</span></div>
-                <input class="input-text" type="text" required>
+            <form action="#" method="get">
+                <div>
+                    <div>item title<span class="red">*</span></div>
+                    <input class="input-text" type="text" required>
 
-                <div>budget<span class="red">*</span></div>
-                <input class="input-text small-input" type="text" required>
-                <span>.</span>
-                <input class="input-text small-input" style="margin: 0;" type="text">
+                    <br><br>
+                    <div>budget<span class="red">*</span></div>
+                    <input class="input-text small-input" type="text" required>
+                    <span>.</span>
+                    <input class="input-text small-input" style="margin: 0;" type="text">
 
-                <div>details<span class="red">*</span></div>
-                <textarea style="min-width: 95%;max-width: 100%;min-height: 124px" class="input-text"
-                          required>
+                    <br><br>
+                    <div>details<span class="red">*</span></div>
+                    <textarea style="min-width: 100%;max-width: 100%;min-height: 124px" class="input-text"
+                              required>
                 </textarea>
 
-                <div>amount</div>
-                <span>unlimited</span>
-                <div class="check">
-                    <div class="checked"></div>
-                </div>
-
-                <br>
-                <input class="input-text" type="text">
-
-                <div>category<span class="red">*</span></div>
-                <input class="input-text small-input" type="text" required>
-                /
-                <input class="input-text small-input" style="margin: 0;" type="text" required>
-
-                <div>Location<span class="red">*</span></div>
-                <input class="input-text small-input" type="text" required>
-
-                <br>
-                <span>Exchangeable</span>
-                <div class="check">
-                    <div class="checked"></div>
-                </div>
-
-                <br>
-                <span>Used</span>
-                <div class="check">
-                    <div class="checked"></div>
-                </div>
-
-                <div class="flex-box" style="flex-wrap: wrap;justify-content: center">
-                    <div v-for="(image,index) in images" v-if="index < 6" class="input-text image-h-upload"
-                         @click="uploadImage">
-                        <!--<div style="width: 100%;text-align: center">Main</div>-->
-                        <input type="file" class="file-input">
-                        <img :name="index" class="add-image" :src="image.url" alt="">
+                    <br><br>
+                    <div>amount</div>
+                    <span>unlimited</span>
+                    <div class="check">
+                        <div class="checked"></div>
                     </div>
-                </div>
 
-            </div>
+                    <br>
+                    <input class="input-text" type="text">
+
+                    <br><br>
+                    <div>category<span class="red">*</span></div>
+                    <input class="input-text small-input" type="text" required>
+                    <span>/</span>
+                    <input class="input-text small-input" style="margin: 0;" type="text" required>
+
+                    <br><br>
+                    <div>Location<span class="red">*</span></div>
+                    <input class="input-text small-input" type="text" required>
+
+                    <br><br>
+                    <span>Exchangeable</span>
+                    <div class="check">
+                        <div class="checked"></div>
+                    </div>
+
+                    <br><br>
+                    <span>Used</span>
+                    <div class="check">
+                        <div class="checked"></div>
+                    </div>
+
+                    <br><br>
+                    <div>Images<span class="red">*</span></div>
+                    <br><br>
+                    <div class="flex-box" style="flex-wrap: wrap;justify-content: center">
+                        <div v-for="(image,index) in images" v-if="index < 6" class="input-text image-h-upload"
+                             @click="uploadImage">
+                            <!--<div style="width: 100%;text-align: center">Main</div>-->
+                            <input type="file" class="file-input">
+                            <img :name="index" class="add-image" :src="image.url" alt="">
+                        </div>
+                    </div>
+
+                    <br>
+                    <button class="input-button" style="width: 100%">SAVE</button>
+                </div>
+            </form>
         </div>
     </div>
 </template>
@@ -91,6 +103,22 @@
                 uploder.onchange = (e) => {
                     let reader = new FileReader();
                     reader.readAsDataURL(e.target.files[0]);
+
+                    let ele = event.target;
+                    reader.onloadstart = () => {
+                        while (!ele.className.includes('image-h-upload')) {
+                            ele = ele.parentElement;
+                        }
+                        /**
+                         * you can add the load holder here!!
+                         *
+                         * */
+                        ele.style.animationName = 'holder';
+                    };
+
+                    reader.onloadend = () => {
+                        ele.style.animationName = '';
+                    };
                     reader.onload = () => {
                         axios.post('/api/upload', {
                             result: reader.result,
@@ -127,6 +155,8 @@
         border-style: dashed;
         border-width: 6px;
         margin-bottom: 24px;
+        animation-duration: 1500ms;
+        animation-iteration-count: infinite;
     }
 
     .file-input {
